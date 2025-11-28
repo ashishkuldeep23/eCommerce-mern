a5a0719a891a97143fcdfa787753e679e7619314
Ashish kuldeep <ashishkuldeep6@gmail.com>
2025-11-28 14:57:40 +0530

    Checking build

--- backEnd_Part/app.js
+++ backEnd_Part/app.js
@@ -39,7 +39,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// // // --->
+// // // CORS Arr of allowed origins --->
 app.use(
   cors({
     credentials: true,
